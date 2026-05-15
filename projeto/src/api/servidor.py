16ad84303f332b487ac13d01c6a6bc0e14a3a311
iguinhodosprograma <igor.christofidis.lima@gmail.com>
2026-05-15 20:50:36 -0300

    fix: debug com valores errados

--- projeto/src/api/servidor.py
+++ projeto/src/api/servidor.py
@@ -127,4 +127,4 @@ def get_clima():
 
 # ── Entry point ────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host = "0.0.0.0", port=5000, debug=True)
